c5135cf4b9fec01f52e919e0b19df5d2eb85c376
Jake Persing <misterdamon@gmail.com>
2015-02-02 23:54:41 -0800

    added buttons and addition/subtraction/division/multiplication operations in

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,9 +11,51 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var resultView: UILabel!
+  var userIsTyping = false
+  var inputStack = [String]()
 
-
-
+  @IBAction func enterDigit(sender: UIButton) {
+    let buttonDigit = sender.currentTitle!
+    
+    if userIsTyping {
+      resultView.text = resultView.text! + buttonDigit
+    } else {
+      resultView.text = buttonDigit
+      userIsTyping = true
+    }
+  }
+  
+  
+  @IBAction func addOperator(sender: UIButton) {
+    
+    inputStack.append(resultView.text!)
+    inputStack.append(sender.currentTitle!)
+    resultView.text = sender.currentTitle!
+    userIsTyping = false
+    println(inputStack)
+  }
+  
+  
+  @IBAction func performOperation(){
+    inputStack.append(resultView.text!)
+    
+    if inputStack.count > 0 {
+      let op1 = (inputStack.removeAtIndex(0) as NSString).doubleValue
+      let operation = inputStack.removeAtIndex(0)
+      let op2 = (inputStack.removeAtIndex(0) as NSString).doubleValue
+      
+      switch operation {
+      case "✖️":  resultView.text = "\(op1 * op2)";
+      case "➗":  resultView.text =  "\(op1 / op2)";
+      case "➕":  resultView.text = "\(op1 + op2)";
+      default: break;
+      }
+    }
+    
+    
+  }
+  
+  
   
 }
 
